f4b6c5a248f181e6b103d62409b8929a70bac33c
Hristina Dakic <hristinadakic@t196-129.demo.tuwien.ac.at>
2019-02-09 15:24:07 +0100

    funny quotes

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
-      var botui = new BotUI('hello-world');
+var botui = new BotUI('hello-world');
 
-      botui.message.add({
-        content: 'Hello World from bot!'
-      }).then(function () { // wait till previous message has been shown.
+botui.message.add({
+  content: 'Hello World from bot!'
+}).then(function () { // wait till previous message has been shown.
 
-        botui.message.add({
-          delay: 1000,
-          human: true,
-          content: 'Hello World from human!'
-        });
-      });
+  botui.message.add({
+    delay: 1000,
+    human: true,
+    content: 'Hello World from human!'
+  })
+    .then(function () {
+      $.get('http://quotes.rest/qod/categories', {}, function success(response) {
+        botui.action.button({
+          action: [
+            { // show only one button
+              text: 'inspire',
+              value: 'one'
+            },
+            { // show only one button
+              text: 'life',
+              value: 'two'
+            },
+            { // show only one button
+              text: 'funny',
+              value: 'three'
+            }
+          ]
+
+        }).then(function (buttonresult) {
+          $.get('http://quotes.rest/qod.json?category=' + buttonresult.text, {}, function success(response) 
+          { 
+            var quote_obj = response.contents.quotes[0];
+            botui.message.add({
+              content: quote_obj.author + ' says: ' + '"' + quote_obj.quote +'"'
+            }) 
+          })
+
+        })
+      })
+    })
+})
